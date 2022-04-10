c0371087822a6013c21a10b41960bfa3d55e4a0b
SungGil-5125 <sunggil0125@naver.com>
2022-04-10 13:07:07 +0900

    ãAdd EntityManger

--- src/main/java/com/example/hellospring2/SpringConfig.java
+++ src/main/java/com/example/hellospring2/SpringConfig.java
@@ -1,13 +1,30 @@
 package com.example.hellospring2;
 
-import com.example.hellospring2.repository.MemberRepository;
-import com.example.hellospring2.repository.MemoryMemberRepository;
+import com.example.hellospring2.repository.*;
 import com.example.hellospring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource = dataSource;
+//    }
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +33,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepositoty();
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/com/example/hellospring2/service/MemberService.java
+++ src/main/java/com/example/hellospring2/service/MemberService.java
@@ -4,11 +4,13 @@ import com.example.hellospring2.domain.Member;
 import com.example.hellospring2.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //test ctrl + shift + t
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
