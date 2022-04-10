de918921dda060cd8b27acca236732ad6ecc57c9
SungGil-5125 <sunggil0125@naver.com>
2022-04-10 21:50:46 +0900

    스프링 데이터 jpa

--- src/main/java/com/example/hellospring2/repository/SpringDataJpaMemberRepostitory.java
+++ src/main/java/com/example/hellospring2/repository/SpringDataJpaMemberRepostitory.java
@@ -1,2 +1,13 @@
-package com.example.hellospring2.repository;public interface SpringDataJpaMemberRepostitory {
+package com.example.hellospring2.repository;
+
+import com.example.hellospring2.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepostitory extends JpaRepository<Member, Long>, MemberRepository { //인터페이스는 다중 상속 가능
+    //스프링 데이터 jpa가 JpaRepository를 보고 자동으로 구현체를 만들어 준다 /memberRepository를 상속 한다고 해서 바로 쓸수 있나 ?
+
+    @Override
+    Optional<Member> findByName(String name);
 }
